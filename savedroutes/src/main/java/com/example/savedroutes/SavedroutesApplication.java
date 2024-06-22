0dbe107d00faa323514a1146313d05058a43ae8c
Fardin <faa35@sfu.ca>
2024-06-21 20:19:47 -0700

    Initial commit by Fardin

---
+++ savedroutes/src/main/java/com/example/savedroutes/SavedroutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.savedroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavedroutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavedroutesApplication.class, args);
+	}
+
+}
---
+++ savedroutes/src/test/java/com/example/savedroutes/SavedroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.savedroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavedroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
