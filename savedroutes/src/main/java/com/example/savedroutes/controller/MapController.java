474d88974a2c5a4f7635673ad6b77aeebb7b6038
Fardin <faa35@sfu.ca>
2024-06-23 14:35:40 -0700

    added route API

---
+++ savedroutes/src/main/java/com/example/savedroutes/controller/MapController.java
@@ -0,0 +1,14 @@
+package com.example.savedroutes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapController {
+
+    @GetMapping("/")
+    public String showMap(Model model) {
+        return "index";
+    }
+}
