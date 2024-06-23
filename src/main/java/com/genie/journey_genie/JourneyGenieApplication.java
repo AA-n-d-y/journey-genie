a317e7891ab124d3f37acd9b612f154a563fc255
Logan Sadowski <logansadowski5@gmail.com>
2024-06-23 12:15:35 -0700

    project base (no content)

---
+++ src/main/java/com/genie/journey_genie/JourneyGenieApplication.java
@@ -0,0 +1,13 @@
+package com.genie.journey_genie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneyGenieApplication.class, args);
+	}
+
+}
