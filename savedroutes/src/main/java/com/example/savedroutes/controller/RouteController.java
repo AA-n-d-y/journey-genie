6bcda01b3f49213ecf00b8ec1754f4ec61839437
Logan Sadowski <logansadowski5@gmail.com>
2024-06-28 16:25:26 -0700

    feat: display saved routes

--- savedroutes/src/main/java/com/example/savedroutes/controller/RouteController.java
+++ savedroutes/src/main/java/com/example/savedroutes/controller/RouteController.java
@@ -28,6 +28,25 @@ public class RouteController {
     @GetMapping("/route-details/{id}")
     public String viewRouteDetails(@PathVariable Long id, Model model) {
         Route route = routeRepository.findById(id).orElse(null);
+
+        if (route.getStartPoint().contains("#")) {
+            int index = route.getStartPoint().indexOf("#");
+            StringBuilder startPoint = new StringBuilder(route.getStartPoint());
+            startPoint = startPoint.delete(index, index+6);
+            route.setStartPoint(startPoint.toString());
+        }   
+        route.setStartPoint(route.getStartPoint().replace(", ", "+"));
+        route.setStartPoint(route.getStartPoint().replace(" ", "+"));
+
+        if (route.getEndPoint().contains("#")) {
+            int index = route.getEndPoint().indexOf("#");
+            StringBuilder endPoint = new StringBuilder(route.getEndPoint());
+            endPoint = endPoint.delete(index, index+6);
+            route.setEndPoint(endPoint.toString());
+        }
+        route.setEndPoint(route.getEndPoint().replace(", ", "+"));
+        route.setEndPoint(route.getEndPoint().replace(" ", "+"));
+
         model.addAttribute("route", route);
         return "route-details";
     }
