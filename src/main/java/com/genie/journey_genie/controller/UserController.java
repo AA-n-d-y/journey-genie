9260d330a3c02ebdeb6e4a5f51333865527af11e
Brandon <bmw14@sfu.ca>
2024-06-28 15:07:58 -0700

    completed register

--- src/main/java/com/genie/journey_genie/controller/UserController.java
+++ src/main/java/com/genie/journey_genie/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.genie.journey_genie.controller;
 
 import java.util.Map;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 import com.genie.journey_genie.model.User;
 import com.genie.journey_genie.repository.UserRepository;
@@ -23,5 +26,12 @@ public class UserController {
         //String type = newuser.get("type");
         repo.save(new User(username, password, "user"));
         return "redirect:/";
+    }
+
+    @GetMapping("/admin")
+    public String getAllUsers(Model model){
+        List<User> users = repo.findAll();
+        model.addAttribute("user", users);
+        return "usersList";
     }
 }
