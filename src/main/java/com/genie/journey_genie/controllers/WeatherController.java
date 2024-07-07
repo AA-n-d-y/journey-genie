ecdd94a9b569b159c4f77a86cbebdc520814016c
Fardin <faa35@sfu.ca>
2024-07-07 03:33:51 -0700

    hid weather api key with .env

--- src/main/java/com/genie/journey_genie/controllers/WeatherController.java
+++ src/main/java/com/genie/journey_genie/controllers/WeatherController.java
@@ -1,5 +1,6 @@
 package com.genie.journey_genie.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WeatherController {
 
+    @Value("${WEATHER_API_KEY}")
+    private String weatherApiKey;
+
     @GetMapping("/weather")
     public String showWeatherDashboard(Model model) {
+        model.addAttribute("weatherApiKey", weatherApiKey);
         return "weather";
     }
 }
