7b51a19b417e936959adaf779918a640b9368651
Andy <andyhuang9338@gmail.com>
2024-07-24 13:04:33 -0700

    features/checklist: added an update button

--- src/main/java/com/genie/journey_genie/controllers/ChecklistController.java
+++ src/main/java/com/genie/journey_genie/controllers/ChecklistController.java
@@ -18,7 +18,6 @@ import com.genie.journey_genie.models.User;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-//import com.genie.journey_genie.models.Checklist;
 import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -59,6 +58,7 @@ public class ChecklistController {
             model.addAttribute("checklistID", id);
             model.addAttribute("act", null);
             model.addAttribute("places", null);
+            response.setStatus(200);
             return "checklist";
         }
 
@@ -67,6 +67,7 @@ public class ChecklistController {
         model.addAttribute("checklistID", id);
         model.addAttribute("act", activities);
         model.addAttribute("places", routeForAssocChecklist.getChecklist().getPlaces());
+        response.setStatus(200);
         return "checklist";
     }
 
@@ -83,11 +84,12 @@ public class ChecklistController {
         // Else
         Route2 routeForAssocChecklist = routeRepository.findById(id).orElse(null);
         model.addAttribute("route", routeForAssocChecklist);
+        response.setStatus(200);
         return "makeChecklist";
     }
 
 
-    // Post request (creating a checklist) 
+    // Post request (creating a checklist/adding activities to the checklist) 
     @PostMapping("/makeChecklist/{id}")
     public String addToChecklist(@RequestParam Map<String, String> newChecklist, @PathVariable Long id, HttpServletResponse response, HttpSession session) {
         // If not logged in
@@ -103,16 +105,13 @@ public class ChecklistController {
         if(routeForAssocChecklist.getChecklist() == null){
             routeForAssocChecklist.setChecklist(new Checklist());
         }
+
         Checklist checklist = routeForAssocChecklist.getChecklist(); // Get the Checklist object from the Route object
         checklist.getActivities().add(activity); // Add the activity to the Checklist object
         checklist = checklistRepository.save(checklist); // Save the Checklist object
-
-        
         routeForAssocChecklist.setChecklist(checklist);
-
-
         routeRepository.save(routeForAssocChecklist);
-
+        response.setStatus(201);
         return "redirect:/makeChecklist/" + id;
     }
 
@@ -145,6 +144,7 @@ public class ChecklistController {
             routeRepository.save(route); // Save the route
         }
         
+        response.setStatus(201);
         return "redirect:/route-details/" + id;
     }
  
@@ -164,6 +164,7 @@ public class ChecklistController {
         checklistRepository.save(route.getChecklist()); // Saving the checklist
         route.setChecklist(route.getChecklist());
         routeRepository.save(route); // Saving the route
+        response.setStatus(200);
         return "redirect:/makeChecklist/checklist/" + id;
     }
 
@@ -183,8 +184,26 @@ public class ChecklistController {
         checklistRepository.save(route.getChecklist()); // Saving the checklist
         route.setChecklist(route.getChecklist());
         routeRepository.save(route); // Saving the route
+        response.setStatus(200);
         return "redirect:/makeChecklist/checklist/" + id;
     }
 
 
+    // Post request (saving an activity after editing it)
+    @PostMapping("/updateActivity/{id}")
+    public String updateActivity(@RequestParam Map<String, String> activity, @PathVariable Long id, HttpServletResponse response, HttpSession session) {
+
+        response.setStatus(200);
+        return "redirect:/makeChecklist/checklist/" + id;
+    }
+
+
+    // Post request (saving a place after updating it)
+    @PostMapping("/updatePlace/{id}")
+    public String updatePlace(@RequestParam Map<String, String> place, @PathVariable Long id, HttpServletResponse response, HttpSession session) {
+
+        response.setStatus(200);
+        return "redirect:/makeChecklist/checklist/" + id;
+    }
+    
 }
